bd4c30ef77d6b1d79e452ef2a3a563f846fce3ea
Marinarty <tsiganosha@yandex.ru>
2016-06-25 21:23:13 +0600

    все доделано

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,9 +23,9 @@ module.exports = function(grunt) {
               "last 2 Firefox versions",
               "last 2 Opera versions",
               "last 2 Edge versions"
-            ]})
+            ]}),
             require("css-mqpacker")({
-              sort: true
+              sort: false
             })
           ]
         },
@@ -90,7 +90,7 @@ module.exports = function(grunt) {
           ],
          dest: "build"
        }]
-     }
+     },
      html: {
        files: [{
          expand: true,
@@ -152,71 +152,3 @@ module.exports = function(grunt) {
   ]);
 
 };
-
-
-
-// "use strict";
-//
-// module.exports = function(grunt) {
-//   grunt.loadNpmTasks("grunt-contrib-less");
-//   grunt.loadNpmTasks("grunt-browser-sync");
-//   grunt.loadNpmTasks("grunt-contrib-watch");
-//   grunt.loadNpmTasks("grunt-postcss");
-//
-//   grunt.initConfig({
-//     less: {
-//       style: {
-//         files: {
-//           "css/style.css": "less/style.less"
-//         }
-//       }
-//     },
-//
-//     postcss: {
-//       style: {
-//         options: {
-//           processors: [
-//             require("autoprefixer")({browsers: [
-//               "last 1 version",
-//               "last 2 Chrome versions",
-//               "last 2 Firefox versions",
-//               "last 2 Opera versions",
-//               "last 2 Edge versions"
-//             ]})
-//           ]
-//         },
-//         src: "css/*.css"
-//       }
-//     },
-//
-//     browserSync: {
-//       server: {
-//         bsFiles: {
-//           src: [
-//             "*.html",
-//             "css/*.css"
-//           ]
-//         },
-//         options: {
-//           server: ".",
-//           watchTask: true,
-//           notify: false,
-//           open: true,
-//           ui: false
-//         }
-//       }
-//     },
-//
-//     watch: {
-//       style: {
-//         files: ["less/**/*.less"],
-//         tasks: ["less", "postcss"],
-//         options: {
-//           spawn: false
-//         }
-//       }
-//     }
-//   });
-//
-//   grunt.registerTask("serve", ["browserSync", "watch"]);
-// };
